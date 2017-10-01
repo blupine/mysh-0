a4d1dc9d55929c1d1ae67c3dbd83112bdcbbf224
skwint11@gmail.com <skwint11@gmail.com>
2017-10-01 10:45:00 +0900

    Shell command parser

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,24 @@
 #include "utils.h"
-
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
+#include <stdlib.h>
+#include <string.h>
+void mysh_parse_command(const char* command, int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	int command_count = 1;
+	char copy_buf[8096];
+	char * parse;
+	*argv = (char**)malloc(sizeof(char) * 4);
+	strcpy(copy_buf, command);
+
+	parse = strtok(copy_buf, " \t\n");
+
+	while (parse != NULL)
+	{
+		*(*argv + command_count - 1) = (char*)malloc(sizeof(char)*strlen(parse));
+		strcpy(*(*argv + command_count - 1), parse);
+		
+		parse = strtok(NULL, " \t\n");
+		if (parse == NULL) break;
+		command_count++;
+	}
+	*argc = command_count;
 }
