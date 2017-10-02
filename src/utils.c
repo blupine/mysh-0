d79b23aa2f756d87cb57d58734506c273ef2379a
skwint11@gmail.com <skwint11@gmail.com>
2017-10-02 16:56:03 +0900

    Empty command case modified

--- src/utils.c
+++ src/utils.c
@@ -3,22 +3,33 @@
 #include <string.h>
 void mysh_parse_command(const char* command, int *argc, char*** argv)
 {
-	int command_count = 1;
+	int command_count = 0;
 	char copy_buf[8096];
 	char * parse;
-	*argv = (char**)malloc(sizeof(char) * 4);
+
+	*argv = (char**)malloc(sizeof(char)*100);
 	strcpy(copy_buf, command);
+	copy_buf[strlen(command)] = '\0';
+	parse = strtok(copy_buf, " ");
 
-	parse = strtok(copy_buf, " \t\n");
-
-	while (parse != NULL)
+	while (parse!=NULL)
 	{
-		*(*argv + command_count - 1) = (char*)malloc(sizeof(char)*strlen(parse));
-		strcpy(*(*argv + command_count - 1), parse);
+		if(strcmp(parse, "\t") == 0 || strcmp(parse, "\n") == 0 )
+		{
+			parse = strtok(NULL, " ");
+			continue;
+		}
+		*(*argv + command_count) = (char*)malloc(sizeof(char)*strlen(parse)+1);
+		strcpy(*(*argv + command_count), parse);
 		
-		parse = strtok(NULL, " \t\n");
-		if (parse == NULL) break;
+		parse = strtok(NULL, " ");		
 		command_count++;
+	}
+	if(command_count==0)
+	{
+		*(*argv) = (char*)malloc(sizeof(char));
+		command_count = 1;
+		strcpy(**argv, "");
 	}
 	*argc = command_count;
 }
