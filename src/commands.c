5ec6273a12b773c12cd915e12f117c1912afbc07
skwint11@gmail.com <skwint11@gmail.com>
2017-10-03 23:40:46 +0900

    Add cd validate test exception handling

--- src/commands.c
+++ src/commands.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include "commands.h"
 #include <sys/stat.h>
-#include <sys/types.h>
+//#include <sys/types.h>
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
