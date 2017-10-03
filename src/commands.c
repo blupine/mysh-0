bab9af3ba0df858dc50ba22b06f007a91f59840f
skwint11@gmail.com <skwint11@gmail.com>
2017-10-03 23:38:40 +0900

    Add cd validate test exception

--- src/commands.c
+++ src/commands.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include <unistd.h>
 #include "commands.h"
-
+#include <sys/stat.h>
+#include <sys/types.h>
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
@@ -26,8 +27,16 @@ int do_pwd(int argc, char** argv) {
 }
 
 int validate_cd_argv(int argc, char** argv) {
-	if (argc == 2 && strcmp(*argv, "cd")==0 && access(*(argv+1), F_OK) ==0)
-		return 1;
+	struct stat buf;
+	if (argc == 2 && strcmp(*argv, "cd")==0 && access(*(argv+1), F_OK | R_OK ) ==0)
+	{
+		if( stat(*(argv+1), &buf) < 0 )
+		{	return 0;}
+		if(S_ISDIR(buf.st_mode))
+		{	return 1;}
+		else
+			return 0;
+	}
 	else
 		return 0;
 }
